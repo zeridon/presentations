7e687b18b013dc7a32f0194f8e50e4f2f95abdfb
Vladimir Vitkov <vvitkov@linux-bg.org>
2014-11-11 15:06:00 +0200

    adding support for handouts and notes

--- bin/gen-artifacts.sh
+++ bin/gen-artifacts.sh
@@ -18,7 +18,9 @@ pandoc \
 	--from markdown \
 	--to s5 \
 	--standalone \
+	--self-contained \
 	--smart \
+	--slide-level 2 \
 	--variable s5-url:${_scriptdir}/../assets/s5/amsterdam \
 	${_src} -o ${_dir}/${_base}-s5.html
 
@@ -35,3 +37,31 @@ pandoc \
 	--slide-level 2 \
 	--latex-engine=pdflatex \
 	${_src} -o ${_dir}/${_base}-beamer.pdf
+
+# pdf handouts
+pandoc \
+	--from markdown \
+	--to beamer \
+	--standalone \
+	--smart \
+	--template ${_scriptdir}/../assets/beamer/beamer-custom.template \
+	--variable theme:Amsterdam \
+	--variable fonttheme:professionalfonts \
+	--variable lang:bulgarian \
+	--variable handout:TRUE \
+	--slide-level 2 \
+	--latex-engine=pdflatex \
+	${_src} -o ${_dir}/${_base}-beamer-handouts.pdf
+
+pdfnup \
+	--quiet \
+	--nup 1x2 \
+	--no-landscape \
+	--keepinfo \
+	--paper a4paper \
+	--frame true \
+	--scale 0.9 \
+	--suffix 'nup' \
+	${_dir}/${_base}-beamer-handouts.pdf
+
+mv ${_dir}/${_base}-beamer-handouts-nup.pdf ${_dir}/${_base}-beamer-handouts.pdf
