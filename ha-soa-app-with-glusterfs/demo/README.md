88f5f63220ad480e1efed315e2bfef2200f6436f
Vladimir Vitkov <vvitkov@linux-bg.org>
2015-08-10 12:32:17 +0300

    ha-soa-app-with-glusterfs - Demos
     * Small readme
     * Some rudimentary app documentation

---
+++ ha-soa-app-with-glusterfs/demo/README.md
@@ -0,0 +1,9 @@
+GlusterFS Demo setup
+====================
+This demo is based on vagrant. It starts 3 boxes:
+ * HAProxy load balancer (172.20.20.10)
+ * 2x GlusterFS cluster members
+
+On Initial start it will create VM's, install gluster and configure a base server.
+
+The demo application is mostly useless but serves to demonstrate the principles of replication and updating the local state of data on change from the remote end.
--- ha-soa-app-with-glusterfs/demo/app/run.py
+++ ha-soa-app-with-glusterfs/demo/app/run.py
@@ -1,23 +1,32 @@
+#!/usr/bin/env python
+
+# Import Flask ande define app
 from flask import Flask, redirect
 app = Flask(__name__)
 
+# pull necessities so le can create some payload
 import sys
 from os import listdir
 from os.path import isfile, join
 target = sys.argv[1]
+
+# Simulate app data by listing dir content
 files = [ f for f in listdir(target) if isfile(join(target,f)) ]
 
+# reload config (e.g. list dir again)
 @app.route('/update')
 def populate():
     global files, target
     files = [ f for f in listdir(target) if isfile(join(target,f)) ]
     return 'ok\n'
 
+# serve some data
 @app.route('/')
 def respond():
     global files
     return '\n'.join(files) + '\n'
 
+# Add data (we redirect to update as we are lasy to patch the list)
 @app.route('/add/<name>')
 def create_file(name):
     file = open(join(target,name), 'w')
